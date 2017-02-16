ead8bc03b1d66b17d76ff741dc5b8ed839435b4b
KNIVES-PC\artem <a.panfilov@globus-ltd.com>
2017-02-16 19:22:00 +0300

    hm4 without ***

---
+++ src/index.js
@@ -0,0 +1,145 @@
+/* ДЗ 4 - работа с DOM */
+
+/**
+ * Функция должна создать элемент с тегом DIV, поместить в него текстовый узел и вернуть получившийся элемент
+ *
+ * @param {string} text - текст, который необходимо поместить в div
+ * @return {Element}
+ */
+function createDivWithText(text) {
+    var div = document.createElement('div');
+
+    div.innerHTML = text
+
+    return div;
+}
+
+/**
+ * Функция должна создать элемент с тегом A, установить значение для атрибута href и вернуть получившийся элемент
+ *
+ * @param {string} hrefValue - значение для атрибута href
+ * @return {Element}
+ */
+function createAWithHref(hrefValue) {
+    var a = document.createElement('a');
+
+    a.href = hrefValue;
+
+    return a;
+}
+
+/**
+ * Функция должна вставлять элемент what в начало элемента where
+ *
+ * @param {Element} what - что вставлять
+ * @param {Element} where - куда вставлять
+ */
+function prepend(what, where) {
+    where.insertBefore(what, where.firstChild);
+}
+
+/**
+ * Функция должна перебрать все дочерние элементы элемента where
+ * и вернуть массив, состоящий из тех дочерних элементов
+ * следующим соседом которых является элемент с тегом P
+ * Рекурсия - по желанию
+ *
+ * @param {Element} where - где искать
+ * @return {Array<Element>}
+ *
+ * @example
+ * для html '<div></div><p></p><a></a><span></span><p></p>'
+ * функция должна вернуть: [div, span]
+ * т.к. следующим соседом этих элементов является элемент с тегом P
+ */
+function findAllPSiblings(where) {
+    var chilren = where.children;
+    var resAr = [];
+
+    for (var ch of chilren) {
+        if (ch.nextElementSibling) {
+            if (ch.nextElementSibling.tagName === 'P') {
+                resAr.push(ch);
+            }
+        }
+    }
+
+    return resAr;
+}
+
+/**
+ * Функция должна перебрать все дочерние узлы типа "элемент" внутри where
+ * и вернуть массив, состоящий из текстового содержимого перебираемых элементов
+ * Но похоже, что в код закралась ошибка, которую нужно найти и исправить
+ *
+ * @param {Element} where - где искать
+ * @return {Array<string>}
+ */
+function findError(where) {
+    var result = [];
+
+    for (var child of where.children) {
+        result.push(child.innerText);
+    }
+
+    return result;
+}
+
+/**
+ * Функция должна перебрать все дочерние узлы элемента where
+ * и удалить из него все текстовые узлы
+ * Без рекурсии!
+ * Будьте внимательны при удалении узлов,
+ * можно получить неожиданное поведение при переборе узлов
+ *
+ * @param {Element} where - где искать
+ *
+ * @example
+ * после выполнения функции, дерево <div></div>привет<p></p>loftchool!!!
+ * должно быть преобразовано в <div></div><p></p>
+ */
+function deleteTextNodes(where) {
+    var childNode = where.childNodes;
+
+    for (var child of childNode) {
+        if (child.nodeType == 3) {
+            child.remove();
+        }
+    }
+
+}
+
+/**
+ * Выполнить предудыщее задание с использование рекурсии
+ * то есть необходимо заходить внутрь каждого дочернего элемента
+ *
+ * @param {Element} where - где искать
+ *
+ * @example
+ * после выполнения функции, дерево <span> <div> <b>привет</b> </div> <p>loftchool</p> !!!</span>
+ * должно быть преобразовано в <span><div><b></b></div><p></p></span>
+ */
+function deleteTextNodesRecursive(where) {
+    let childNode = where.childNodes;
+
+    for (let child of childNode) {
+        if (child.childNodes) {
+            deleteTextNodesRecursive(child);
+        }
+    }
+    for (let child of childNode) {
+        if (child.nodeType === 3) {
+            child.remove();
+        }
+    }
+}
+
+export {
+    createDivWithText,
+    createAWithHref,
+    prepend,
+    findAllPSiblings,
+    findError,
+    deleteTextNodes,
+    deleteTextNodesRecursive,
+};
--- test/index.js
+++ test/index.js
@@ -1,7 +1,110 @@
 import assert from 'assert';
+import {
+    createDivWithText,
+    createAWithHref,
+    prepend,
+    findAllPSiblings,
+    findError,
+    deleteTextNodes,
+    deleteTextNodesRecursive,
+} from '../src/index';
 
-describe('Test', () => {
-    it('should work', () => {
-        assert(true == true);
+describe('ДЗ 4 - Работа с DOM', () => {
+    describe('createDivWithText', () => {
+        it('должна возвращать элемент с тегом DIV', () => {
+            let text = 'привет!';
+            let result = createDivWithText(text);
+
+            assert(result instanceof Element);
+            assert.equal(result.tagName, 'DIV');
+        });
+
+        it('должна добавлять текст в элемент', () => {
+            let text = 'привет!';
+            let result = createDivWithText(text);
+
+            assert.equal(result.innerText, text);
+        });
     });
+
+    describe('createAWithHref', () => {
+        it('должна возвращать элемент с тегом A', () => {
+            let href = 'http://loftschool.com';
+            let result = createAWithHref(href);
+
+            assert(result instanceof Element);
+            assert.equal(result.tagName, 'A');
+        });
+
+        it('должна добавлять атрибут href', () => {
+            let href = 'http://loftschool.com';
+            let result = createAWithHref(href);
+
+            assert.equal(result.getAttribute('href'), href);
+        });
+    });
+
+    describe('prepend', () => {
+        it('должна добавлять элемента в начало', () => {
+            let where = document.createElement('div');
+            let what = document.createElement('p');
+
+            where.innerHTML = ', <b>loftschool</b>!';
+            what.innerText = 'привет';
+
+            prepend(what, where);
+
+            assert.equal(where.firstChild, what);
+            assert.equal(where.innerHTML, '<p>привет</p>, <b>loftschool</b>!');
+        });
+    });
+
+    describe('findAllPSiblings', () => {
+        it('должна возвращать массив с элементами, соседями которых являются P', () => {
+            let where = document.createElement('div');
+            let result;
+
+            where.innerHTML = '<div></div><p></p><span></span><span></span><p></p>';
+            result = findAllPSiblings(where);
+
+            assert(Array.isArray(result));
+            assert.deepEqual(result, [where.children[0], where.children[3]]);
+        });
+    });
+
+    describe('findError', () => {
+        it('должна возвращать массив из текстового содержимого элементов', () => {
+            let where = document.createElement('div');
+            let result;
+
+            where.innerHTML = ' <div>привет</div>, <div>loftschool</div>!!!';
+            result = findError(where);
+
+            assert(Array.isArray(result));
+            assert.deepEqual(result, ['привет', 'loftschool']);
+        });
+    });
+
+    describe('deleteTextNodes', () => {
+        it('должна удалить все текстовые узлы', () => {
+            let where = document.createElement('div');
+
+            where.innerHTML = ' <div></div>привет<p></p>loftchool!!!';
+            deleteTextNodes(where);
+
+            assert.equal(where.innerHTML, '<div></div><p></p>');
+        });
+    });
+
+    describe('deleteTextNodesRecursive', () => {
+        it('должна рекурсивно удалить все текстовые узлы', () => {
+            let where = document.createElement('div');
+
+            where.innerHTML = '<span> <div> <b>привет</b> </div> <p>loftchool</p> !!!</span>';
+            deleteTextNodesRecursive(where);
+
+            assert.equal(where.innerHTML, '<span><div><b></b></div><p></p></span>');
+        });
+    });
+
 });
